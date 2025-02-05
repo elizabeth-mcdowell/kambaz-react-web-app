e214a7b54f813159d2524c15b8816a4961ada3ef
Elizabeth McDowell <mcdowell.e@northeastern.edu>
2025-02-05 17:49:16 -0500

    A2

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -3,16 +3,12 @@ import { Button, Col, Row } from 'react-bootstrap';
 export default function AssignmentEditor() {
   return (
     <div className="ms-4" id="wd-assignments-editor">
-      <Row>
-        <label htmlFor="wd-name">Assignment Name</label>
-      </Row>
-      <Row>
-        <input className="form-control" type="text" id="wd-name" value="A1" />
-      </Row>
+      <Row><label htmlFor="wd-name">Assignment Name</label></Row>
+      <Row><input className="form-control" type="text" id="wd-name" value="A1" /></Row>
+
       <Row className="mt-3">
-        <textarea className="form-control" id="wd-name" rows={6}>
-          The assignment is available online. Submit a link to the landing page
-          of your Web application running on Netlify of
+        <textarea className="form-control" id="wd-description" rows={6}>
+        The assignment is available online Submit a link to the landing page of
         </textarea>
       </Row>
       <Row className="mt-3">
@@ -38,12 +34,8 @@ export default function AssignmentEditor() {
         </Col>
         <Col>
           <select className="form-select" id="wd-group">
-            <option selected value="ASSIGNMENTS">
-              Assignments
-            </option>
-            <option value="QUIZZES">Quizzes</option>
-            <option value="EXAMS">Exams</option>
-            <option value="PROJECT">Project</option>
+            <option selected value="ASSIGNMENTS"> Assignments</option>
+            <option value="OTHER">Other</option>
           </select>
         </Col>
       </Row>
@@ -55,28 +47,25 @@ export default function AssignmentEditor() {
         </Col>
         <Col>
           <select className="form-select" id="wd-display-grade-as">
-            <option selected value="PERCENTAGE">
-              Percentage
-            </option>
-            <option value="POINTS">Points</option>
-            <option value="LETTER">Letter Grade</option>
+            <option selected value="PERCENTAGE">Percentage</option>
+            <option value="FRACTION">Fraction</option>
+            <option value="OTHER">Other</option>
           </select>
         </Col>
       </Row>
       <Row className="mt-3">
         <Col xs={3}>
-          <label className="float-end">Submission Type</label>
+          <label className="float-end" id="wd-submission-type">Submission Type</label>
         </Col>
         <Col>
           <div className="form-control">
             <select className="form-select mt-2" id="wd-submission-type">
-              <option selected value="ONLINE">
-                Online
-              </option>
-              <option value="PHYSICAL">Physical Copy</option>
+              <option selected value="ONLINE">Online</option>
+              <option value="INPERSON">In Person</option>
+              <option value="OTHER">Other</option>
             </select>
             <br />
-            <strong className="mt-3">Online Entry Options</strong>
+            <strong className="mt-3">Online Entry Type:</strong>
             <div className="form-check mt-2">
               <input
                 className="form-check-input"
@@ -147,12 +136,10 @@ export default function AssignmentEditor() {
             <label htmlFor="wd-assign-to">
               <strong className="mt-2">Assign to</strong>
             </label>
-            <input
-              className="form-control"
-              type="text"
-              id="wd-points"
-              value="Everyone"
-            />
+            <select className="form-select" id="wd-assign-to">
+              <option selected value="EVERYONE">Everyone</option>
+              <option value="OTHER">Other</option>
+            </select>
             <label className="mt-2" htmlFor="wd-due-date">
               <strong>Due</strong>
             </label>
@@ -171,18 +158,18 @@ export default function AssignmentEditor() {
                   className="form-control"
                   type="date"
                   id="wd-available-from"
-                  value="2025-05-06"
+                  value="2000-01-21"
                 />
               </div>
               <div style={{ width: '100%' }}>
-                <label className="mt-2" htmlFor="wd-until">
+                <label className="mt-2" htmlFor="wd-available-until">
                   <strong>Until</strong>
                 </label>
                 <input
                   className="form-control"
                   type="date"
-                  id="wd-until"
-                  value="2025-05-20"
+                  id="wd-available-until"
+                  value="2000-01-21"
                 />
               </div>
             </div>
